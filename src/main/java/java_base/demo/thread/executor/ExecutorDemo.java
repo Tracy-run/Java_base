ea1c2665014dda220662a1caafd4496785eb73e1
Tracy--RUN <2463136713@qq.com>
2019-08-13 15:44:55 +0800

    4 type of thread-pool method

---
+++ src/main/java/java_base/demo/thread/executor/ExecutorDemo.java
@@ -0,0 +1,109 @@
+package java_base.demo.thread.executor;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+public class ExecutorDemo{
+
+
+    public static void main(String [] args){
+
+
+
+    }
+
+    public void executorPool(){
+
+        /**
+         *  executors first pool type
+         *
+         */
+        ExecutorService executorService  = Executors.newFixedThreadPool(3);
+
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                //todo
+            }
+        });
+
+        Future<?> submit = executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+                //todo
+            }
+        });
+
+        /**
+         * executors second pool type
+         *
+         */
+        ExecutorService executorSingle = Executors.newSingleThreadExecutor();
+
+        executorSingle.execute(new Runnable() {
+            @Override
+            public void run() {
+                //todo
+            }
+        });
+
+        Future<?> result = executorSingle.submit(new Runnable() {
+            @Override
+            public void run() {
+                //todo
+            }
+        });
+
+        /**
+         *  executors third pool type
+         *
+         */
+        ExecutorService executorCache = Executors.newCachedThreadPool();
+
+        Future<?> submit1 = executorCache.submit(new Runnable() {
+            @Override
+            public void run() {
+                //todo     
+            }
+        });
+
+        executorCache.execute(new Runnable() {
+            @Override
+            public void run() {
+                //todo
+            }
+        });
+
+        /**
+         *  executors fourth pool type
+         *
+         */
+        ExecutorService schaduledPool = Executors.newScheduledThreadPool(5);
+
+        schaduledPool.execute(new Runnable() {
+            @Override
+            public void run() {
+                //todo
+            }
+        });
+
+        Future submit2 = schaduledPool.submit(new Callable() {
+            @Override
+            public Object call() throws Exception {
+                //todo
+                return null;
+            }
+        });
+
+
+    }
+
+}
+
+
+
+
+
+
